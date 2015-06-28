2ad0556af9150badd5fd0cd8a8b06e2169b26497
Jose Alava <jalava85@gmail.com>
2015-06-28 15:07:09 -0400

    Refactored the gulp into the server folder, it seems that having two gulps was causing issues when running tasks.

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -1,18 +1,76 @@
 /**
- * Created by Jose on 6/11/2015.
+ * Created by Jose on 5/2/2015.
  */
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-var gutil = require('gulp-util');
-var print = require('gulp-print');
+var gulp = require('gulp'),
+    karma = require('karma').server,
+    config = require('./../client/gulp.config.js')(),
+    $ = require('gulp-load-plugins')({lazy: true}),
+    args = require('yargs').argv;
 
-gulp.task('test', function() {
-    return gulp.src(['tests/**/*.js'], { read: false })
-        .pipe(print())
-        .pipe(mocha({ reporter: 'list' }))
-        .on('error', gutil.log);
+//var mocha = require('gulp-mocha');
+var gutil = require('gulp-util');
+//var print = require('gulp-print');
+
+/**
+ * Run test once and exit
+ */
+gulp.task('test', function (done) {
+    karma.start({
+        configFile: __dirname + '/../client/karma.conf.js',
+        singleRun: true
+    }, done);
+});
+
+gulp.task('vet', function () {
+    log('Analyzing source with JSHint and JSCS');
+
+    return gulp
+        .src(config.allTheJs)
+        .pipe($.if(args.verbose, $.print()))
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
+        .pipe($.jshint.reporter('fail'))
+        .pipe($.jscs());
+});
+
+/**
+ * Wire the client side dependencies into the index.html
+ */
+gulp.task('wiredep', function () {
+    var wiredep = require('wiredep').stream,
+        //options = config.getWiredepDefaultOptions();
+        options = config.wiredepOptions;
+    log('starting wiredep');
+    return gulp
+        .src(config.locationOfIndexHtml) // Find the html we will throw into the pipeline
+        .pipe(wiredep(options))
+        .pipe($.inject(gulp.src(config.allTheJs), {relative: true}))
+        .pipe(gulp.dest(config.destinationOfWiredepedFile)); // Dump the resulting file into this folder
 });
 
 gulp.task('watch', function () {
+    gulp.watch([config.allTheJs, config.allTheTestsJs], ['test']);
+});
+
+gulp.task('serverTest', function() {
+    return gulp.src(['tests/**/*.js'], { read: false })
+        .pipe($.print())
+        .pipe($.mocha({ reporter: 'list' }))
+        .on('error', gutil.log);
+});
+
+gulp.task('serverWatch', function () {
     gulp.watch(['./tests/**/*.js', './lib/**/*.js'], ['test']);
 });
+
+function log(msg) {
+    if (typeof(msg) === 'object') {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                $.util.log($.util.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        $.util.log($.util.colors.blue(msg));
+    }
+}
