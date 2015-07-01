72b96c5d857ade6679d4af849513c0225d0a260d
Jose Alava <jalava85@gmail.com>
2015-07-01 00:15:25 -0400

    Added a configuration parameter that contains both an oracle to determine if the user is allowed to access the route and a configuration array that will determine which routes will be initialized

--- server/lib/modules/expressModule.js
+++ server/lib/modules/expressModule.js
@@ -1,40 +1,114 @@
 /**
  * Created by Jose on 6/13/2015.
  */
-module.exports = function (app, dbToUse, baseRoute) {
-    app.get('/' + baseRoute, function (req, res) {
-        dbToUse.findAll(function (err, result) {
-            res.send(result);
+var _ = require('underscore');
+
+module.exports = function (app, dbToUse, baseRoute, options) {
+    if (_.contains(options.capability, 'R')) {
+        app.get('/' + baseRoute, function (req, res) {
+            options.authPredicate(req.headers.uktena).then(function (isAuth) {
+                if(isAuth) {
+                    dbToUse.findAll(function (err, result) {
+                        res.send(result);
+                    });
+                } else {
+                    res.send(401);
+                }
+            });
         });
-    });
 
-    app.get('/' + baseRoute + '/:id', function (req, res) {
-        var query = {author: req.params.id};
-
-        dbToUse.find(query, function (err, result) {
-            res.send(result);
+        app.get('/' + baseRoute + '/:id', function (req, res) {
+            options.authPredicate(req.headers.uktena).then(function (isAuth) {
+                if(isAuth) {
+                    dbToUse.findById(req.params.id, function (err, result) {
+                        res.send(result);
+                    });
+                } else {
+                    res.send(401);
+                }
+            });
         });
-    });
+    }
 
-    app.post('/' + baseRoute, function (req, res) {
-        dbToUse.insert(req.body, function (err, queryResult) {
-            res.send(queryResult.ops);
+    if (_.contains(options.capability, 'r')) {
+        app.get('/' + baseRoute + '/:id', function (req, res) {
+            options.authPredicate(req.headers.uktena).then(function (isAuth) {
+                if(isAuth) {
+                    dbToUse.findById(req.params.id, function (err, result) {
+                        res.send(result);
+                    });
+                } else {
+                    res.send(401);
+                }
+            });
         });
-    });
+    }
 
-    app.put('/' + baseRoute, function (req, res) {
-        dbToUse.update(req.body, res);
-    });
+    if (_.contains(options.capability, 'c')) {
+        app.post('/' + baseRoute, function (req, res) {
+            options.authPredicate(req.headers.uktena).then(function (isAuth) {
+                if(isAuth) {
+                    dbToUse.insert(req.body, function (err, queryResult) {
+                        res.send(queryResult.ops);
+                    });
+                } else {
+                    res.send(401);
+                }
+            });
+        });
+    }
 
-    app.delete('/' + baseRoute, function (req, res) {
-        dbToUse.remove(req.body, res);
-    });
+    if (_.contains(options.capability, 'u')) {
+        app.put('/' + baseRoute, function (req, res) {
+            options.authPredicate(req.headers.uktena).then(function (isAuth) {
+                if(isAuth) {
+                    dbToUse.update(req.body, res);
+                } else {
+                    res.send(401);
+                }
+            });
+        });
+    }
 
-    app.delete('/' + baseRoute + '/:id', function (req, res) {
-        if (req.body.hasOwnProperty('_id')) {
-            dbToUse.removeById(req.body._id, res);
-        } else {
-            dbToUse.removeAll(res);
-        }
-    });
+    if (_.contains(options.capability, 'd')) {
+        app.delete('/' + baseRoute + '/:id', function (req, res) {
+            options.authPredicate(req.headers.uktena).then(function (isAuth) {
+                if(isAuth) {
+                    if (req.body.hasOwnProperty('_id')) {
+                        dbToUse.removeById(req.body._id, res);
+                    } else {
+                        dbToUse.removeAll(res);
+                    }
+                } else {
+                    res.send(401);
+                }
+            });
+        });
+    }
+
+    if (_.contains(options.capability, 'D')) {
+        app.delete('/' + baseRoute, function (req, res) {
+            options.authPredicate(req.headers.uktena).then(function (isAuth) {
+                if(isAuth) {
+                    dbToUse.remove(req.body, res);
+                } else {
+                    res.send(401);
+                }
+            });
+        });
+
+        app.delete('/' + baseRoute + '/:id', function (req, res) {
+            options.authPredicate(req.headers.uktena).then(function (isAuth) {
+                if(isAuth) {
+                    if (req.body.hasOwnProperty('_id')) {
+                        dbToUse.removeById(req.body._id, res);
+                    } else {
+                        dbToUse.removeAll(res);
+                    }
+                } else {
+                    res.send(401);
+                }
+            });
+        });
+    }
 };
